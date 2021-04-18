e05e81184c8d1ceb60fc60e13cb84baeacf1b8f3
Madison Vega <madison.r.vega@gmail.com>
2021-04-18 15:44:58 -0500

    added seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,11 +2,11 @@
 
 INSERT INTO department (department_names) VALUES ('HR')
 INSERT INTO department (department_names) VALUES ('Finance')
-INSERT INTO department (department_names) VALUES ('R&D')
-INSERT INTO department (department_names) VALUES ('EXECUTIVE')
 
-INSERT INTO roles (title, salary, department_id) VALUES ('Boss', 400, 1)
-INSERT INTO roles (department_names) VALUES ('HR')
-INSERT INTO roles (department_names) VALUES ('HR')
-INSERT INTO roles (department_names) VALUES ('HR')
+
+INSERT INTO roles (title, salary, department_id) VALUES ('Boss', 400,000.00, 1)
+INSERT INTO roles (title, salary, department_id) VALUES ('Assisstant Boss', 200,000.00, 1)
+INSERT INTO roles (title, salary, department_id) VALUES ('Head Accountant', 350,000.00, 2)
+INSERT INTO roles (title, salary, department_id) VALUES ('Assisstant Accountant', 150,000.00), 2
+
 
